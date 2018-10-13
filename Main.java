764632c7bf9d85e236b4699e02a7314205a386ae
buihuynhduc <43197106+buihuynhduc@users.noreply.github.com>
2018-10-13 09:16:22 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ package btl;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
+import javax.swing.text.html.HTML;
 
 /**
  *
@@ -18,12 +20,93 @@ public class Main {
      * @param args the command line arguments
      * @throws java.io.FileNotFoundException
      */
+   
     public static void main(String[] args) throws FileNotFoundException, IOException {
       DictionaryManagement dm=new DictionaryManagement();
    Dictionary d=new Dictionary();
    DictionaryCommandline dc =new DictionaryCommandline();
    dm.insertFromFile(d);
-   dc.showAllWords(d);
+        System.out.println("1.tra cuu tu");
+        System.out.println("2.tra cuu theo goi y");
+        System.out.println("3.them sua xoa cac tu");
+        System.out.println("4.dua du lieu vao file");
+        System.out.println("5.thoat");
+    Scanner input=new Scanner(System.in);
+         int n;
+     do
+     {System.out.print("nhap lua chon cua ban : ");
+     
+          n=input.nextInt();
+       while(!(n<=5)&&(n>=1))
+       {
+           System.err.print("xin vui long nhap lai lua chon cua ban : ");
+           n=input.nextInt();
+           
+       }
+       switch(n)
+       {
+           case 1:{
+               dm.dictionaryLookup(d);
+               break;
+           }
+           case 2:
+           {
+               System.err.print("chua nang cap");
+               break;
+           }
+           case 3:
+           {
+               System.out.println("1.them tu");
+        System.out.println("2.xoa tu");
+        System.out.println("3.sua tu");
+        int ba=input.nextInt();
+        switch(ba)
+        {
+            case 1:
+            {
+                dm.dictionaryadd(d);
+                dm.dictionaryExportToFile(d);
+                break;
+            }
+            case 2:
+            {
+                dm.dictionaryDelete(d);
+                dm.insertFromFile(d);
+                break;
+            }
+            case 3:
+            {
+                dm.dictionaryrepair(d);
+                dm.insertFromFile(d);
+                break;
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        break;
+        
+           }
+           case 4:
+           {
+               dm.insertFromFile(d);
+               break;
+               
+           }
+           
+               
+           }
+       }while(n!=5);
+               
+        
+   
+        
+  
+ 
   
            
      
