7673a2784ddf56d53776a55010cdc5c533a99234
buihuynhduc <43197106+buihuynhduc@users.noreply.github.com>
2018-10-14 18:43:32 +0700

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -9,5 +9,9 @@ import java.util.ArrayList;
 
 public  class Dictionary {
     public ArrayList<Word> list=new ArrayList<>();
-    
+    public ArrayList<Word> getWord()
+    {
+        return list;
+    }
+   
 }
